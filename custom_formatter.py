5f52215c480b3ee924b46d5ee67f7eaa0e31717f
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-01-25 01:38:27 +0300

    Remove the color map

--- custom_formatter.py
+++ custom_formatter.py
@@ -6,11 +6,11 @@ class CustomFormatter(logging.Formatter):
         output = "%(asctime)s [%(levelname)s] %(message)s"
 
         colors = {
-            logging.DEBUG: "\x1b[38;20m" + output,
-            logging.INFO: "\x1b[38;20m" + output,
-            logging.WARNING: "\x1b[33;20m" + output,
-            logging.ERROR: "\x1b[31;20m" + output,
-            logging.CRITICAL: "\x1b[31;1m" + output
+            logging.DEBUG: output,
+            logging.INFO: output,
+            logging.WARNING: output,
+            logging.ERROR: output,
+            logging.CRITICAL: output
         }
 
         fmt = colors.get(record.levelno)
