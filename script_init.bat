002820ead3f584f8f7e6860c2262b0bcd88eb565
Ruslan Grishechkin <grishechkinruslan@gmail.com>
2024-01-31 01:01:32 +0300

    add creation of schedule task

--- script_init.bat
+++ script_init.bat
@@ -3,5 +3,8 @@ call .\venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-rem создать планировщик задач при заупске компьютера 
-schtasks /create /tn "wot-daily" /tr %CD%\script_daily.bat /sc onstart > wot_init.log
+set PATH_WOT=%CD%
+echo cd /d %PATH_WOT%>>script_daily.bat
+echo call .\venv\Scripts\activate.bat>>script_daily.bat
+echo python main.py>>script_daily.bat
+schtasks /create /tn "wot-daily" /tr %PATH_WOT%\script_daily.bat /sc onstart > wot_init.log
