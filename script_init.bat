799baf7f4e63b639232ada2ce997a68cf57e46f0
Ruslan Grishechkin <grishechkinruslan@gmail.com>
2024-01-30 20:56:10 +0300

    add file

--- script_init.bat
+++ script_init.bat
@@ -2,3 +2,6 @@ python -m venv venv
 call .\venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+
+rem создать планировщик задач при заупске компьютера 
+schtasks /create /tn "wot-daily" /tr %CD%\script_daily.bat /sc onstart > wot_init.log
