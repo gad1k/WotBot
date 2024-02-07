48a3ea3946903ad8c42353eb9b7627a98c2cfaa1
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-02-07 10:36:11 +0300

    Make minor changes to the init script

--- script_init.bat
+++ script_init.bat
@@ -3,8 +3,8 @@ call .\venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-set PATH_WOT=%CD%
-echo cd /d %PATH_WOT%>>script_daily.bat
-echo call .\venv\Scripts\activate.bat>>script_daily.bat
-echo python main.py>>script_daily.bat
-schtasks /create /tn "wot-daily" /tr %PATH_WOT%\script_daily.bat /sc onstart > wot_init.log
+echo cd /d %CD% >> script_daily.bat
+echo call .\venv\Scripts\activate.bat >> script_daily.bat
+echo python main.py >> script_daily.bat
+
+schtasks /create /tn "wot-daily" /tr %CD%\script_daily.bat /sc onstart
