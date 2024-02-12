aae406e40881c121dd86f62616c2d3978780fd1f
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-02-13 00:09:20 +0300

    Update the configuration of the init file

--- script_init.bat
+++ script_init.bat
@@ -1,19 +1,19 @@
 rem Set up a virtual environment
-python -m venv venv
-call .\venv\Scripts\activate.bat
+python -m venv .venv
+call .\.venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
 rem Create a bat file for the task
 echo cd /d %cd% >> script_daily.bat
-echo call .\venv\Scripts\activate.bat >> script_daily.bat
+echo call .\.venv\Scripts\activate.bat >> script_daily.bat
 echo python main.py >> script_daily.bat
 
 rem Define variables for the settings
 set task_name=WotDaily
 set task_command="%cd%\script_daily.bat"
-set task_trigger=Minute
-set task_interval=5
+set task_trigger=Hourly
+set task_interval=1
 set task_owner=%username%
 
 rem Create the task using schtasks command
