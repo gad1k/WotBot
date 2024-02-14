7d82d570b40f60602c49aff0e923fce57282b02b
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-02-14 22:55:31 +0300

    Add the creation of the new vbs script to init file

--- script_init.bat
+++ script_init.bat
@@ -4,17 +4,22 @@ call .\.venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-rem Create a bat file for the task
-echo cd /d %cd% >> script_daily.bat
-echo call .\.venv\Scripts\activate.bat >> script_daily.bat
-echo python main.py >> script_daily.bat
-
 rem Define variables for the settings
 set task_name=WotDaily
-set task_command="%cd%\script_daily.bat"
+set task_daily="%cd%\script_daily.bat"
+set task_schedule="wscript %cd%\script_schedule.vbs"
 set task_trigger=Hourly
 set task_interval=1
 set task_owner=%username%
 
+rem Create a bat file for the task
+echo cd /d %cd% > script_daily.bat
+echo call .\.venv\Scripts\activate.bat >> script_daily.bat
+echo python main.py >> script_daily.bat
+
+rem Create a vbs file for the schedule
+echo set WshShell = CreateObject("WScript.Shell") > script_schedule.vbs
+echo WshShell.run %task_daily%, 0, True >> script_schedule.vbs
+
 rem Create the task using schtasks command
-schtasks /create /tn %task_name% /tr %task_command% /sc %task_trigger% /mo %task_interval% /ru %task_owner% /f
+schtasks /create /tn %task_name% /tr %task_schedule% /sc %task_trigger% /mo %task_interval% /ru %task_owner% /f
