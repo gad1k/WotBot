15c11b4fb5e3e5871b6061118df15e1dbbbef5c1
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-02 23:10:00 +0300

    Add setUp method to the handler tests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -9,13 +9,18 @@ from handler import TelegramHandler
 class TestExporter(TestCase):
     @mock.patch.object(TelegramExporter, "try_upload_cached_chat_id")
     @mock.patch.object(TelegramExporter, "try_retrieve_chat_id_remotely")
-    @mock.patch.object(TelegramExporter, "send_message")
-    def test_emit(self, mock_send_message, mock_try_retrieve_chat_id_remotely, mock_try_upload_cached_chat_id):
-        handler = TelegramHandler("dummy_token")
+    def setUp(self, mock_try_retrieve_chat_id_remotely, mock_try_upload_cached_chat_id):
+        self.handler = TelegramHandler("dummy_token")
 
+        mock_try_upload_cached_chat_id.assert_called_once()
+        mock_try_retrieve_chat_id_remotely.assert_called_once()
+
+
+    @mock.patch.object(TelegramExporter, "send_message")
+    def test_emit(self, mock_send_message):
         attrs = {
             "name": "dummy_name",
-            "level": 2,
+            "level": 20,
             "pathname": "dummy_path",
             "lineno": 2,
             "msg": "dummy_message",
@@ -23,18 +28,9 @@ class TestExporter(TestCase):
             "exc_info": None
         }
 
-        handler.emit(logging.LogRecord(**attrs))
-
-        mock_try_upload_cached_chat_id.assert_called_once()
-        mock_try_retrieve_chat_id_remotely.assert_called_once()
+        self.handler.emit(logging.LogRecord(**attrs))
         mock_send_message.assert_called_once()
 
 
-    @mock.patch.object(TelegramExporter, "try_upload_cached_chat_id")
-    @mock.patch.object(TelegramExporter, "try_retrieve_chat_id_remotely")
-    def test_init_handler(self, try_retrieve_chat_id_remotely, mock_try_upload_cached_chat_id):
-        handler = TelegramHandler("dummy_token")
-
-        self.assertIsInstance(handler.telegram_exporter, TelegramExporter)
-        mock_try_upload_cached_chat_id.assert_called_once()
-        try_retrieve_chat_id_remotely.assert_called_once()
+    def test_init_handler(self):
+        self.assertIsInstance(self.handler.telegram_exporter, TelegramExporter)
