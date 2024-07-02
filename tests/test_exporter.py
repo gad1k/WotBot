f5117c4ccdca484f96f01cfa5bb16ffda8227901
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-02 23:27:16 +0300

    Add setUp method to the exporter tests

--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -6,41 +6,37 @@ from exception import InactiveChatException, InvalidTokenException
 
 
 class TestExporter(TestCase):
+    def setUp(self):
+        self.exporter = TelegramExporter("dummy_token")
+        self.response = mock.MagicMock(spec=Response)
+
+
     @mock.patch("requests.get")
     def test_exception_inactive_chat(self, mock_get):
-        exporter = TelegramExporter("dummy_token")
+        self.response.json.return_value = {"result": []}
+        mock_get.return_value = self.response
 
-        response = mock.MagicMock(spec=Response)
-        response.json.return_value = {"result": []}
-        mock_get.return_value = response
-
-        self.assertRaises(InactiveChatException, exporter.try_retrieve_chat_id_remotely)
+        self.assertRaises(InactiveChatException, self.exporter.try_retrieve_chat_id_remotely)
         mock_get.assert_called_once_with("https://api.telegram.org/botdummy_token/getUpdates")
 
 
     @mock.patch("requests.get")
     def test_exception_invalid_token(self, mock_get):
-        exporter = TelegramExporter("dummy_token")
+        self.response.json.return_value = {"error_code": 404}
+        mock_get.return_value = self.response
 
-        response = mock.MagicMock(spec=Response)
-        response.json.return_value = {"error_code": 404}
-        mock_get.return_value = response
-
-        self.assertRaises(InvalidTokenException, exporter.try_retrieve_chat_id_remotely)
+        self.assertRaises(InvalidTokenException, self.exporter.try_retrieve_chat_id_remotely)
         mock_get.assert_called_once_with("https://api.telegram.org/botdummy_token/getUpdates")
 
 
     def test_exception_missing_cookie(self):
-        exporter = TelegramExporter("dummy_token")
-        exporter.try_upload_cached_chat_id()
+        self.exporter.try_upload_cached_chat_id()
 
 
     @mock.patch("builtins.open", new_callable=mock.mock_open)
     def test_save_settings(self, mock_open):
-        exporter = TelegramExporter("dummy_token")
-
-        with mock.patch.object(exporter, "chat_id", "dummy_chat_id"):
-            exporter.save_settings()
+        with mock.patch.object(self.exporter, "chat_id", "dummy_chat_id"):
+            self.exporter.save_settings()
 
         mock_open.assert_called_once_with(".settings", "w", encoding="utf-8")
         mock_open().write.assert_called_once_with("dummy_chat_id")
@@ -48,11 +44,9 @@ class TestExporter(TestCase):
 
     @mock.patch("requests.get")
     def test_send_message(self, mock_get):
-        exporter = TelegramExporter("dummy_token")
-
         mock_get.return_value = "dummy_response"
-        with mock.patch.object(exporter, "chat_id", "dummy_chat_id"):
-            exporter.send_message("dummy_text")
+        with mock.patch.object(self.exporter, "chat_id", "dummy_chat_id"):
+            self.exporter.send_message("dummy_text")
 
         expected_result = "https://api.telegram.org/botdummy_token/sendMessage?chat_id=dummy_chat_id&text=dummy_text"
         mock_get.assert_called_once_with(expected_result)
@@ -61,26 +55,21 @@ class TestExporter(TestCase):
     @mock.patch.object(TelegramExporter, "save_settings")
     @mock.patch("requests.get")
     def test_try_retrieve_chat_id_remotely(self, mock_get, mock_save_settings):
-        exporter = TelegramExporter("dummy_token")
+        self.response.json.return_value = {"result": [{"message": {"chat": {"id": "dummy_chat_id"}}}]}
+        mock_get.return_value = self.response
 
-        response = mock.MagicMock(spec=Response)
-        response.json.return_value = {"result": [{"message": {"chat": {"id": "dummy_chat_id"}}}]}
-        mock_get.return_value = response
+        self.exporter.try_retrieve_chat_id_remotely()
 
-        exporter.try_retrieve_chat_id_remotely()
-
-        self.assertEqual(exporter.chat_id, "dummy_chat_id")
+        self.assertEqual(self.exporter.chat_id, "dummy_chat_id")
         mock_get.assert_called_once_with("https://api.telegram.org/botdummy_token/getUpdates")
         mock_save_settings.assert_called_once()
 
 
     @mock.patch("builtins.open", new_callable=mock.mock_open)
     def test_try_upload_cached_chat_id(self, mock_open):
-        exporter = TelegramExporter("dummy_token")
-
         mock_open.return_value.readline.return_value = "dummy_token"
 
-        exporter.try_upload_cached_chat_id()
+        self.exporter.try_upload_cached_chat_id()
 
-        self.assertEqual(exporter.chat_id, "dummy_token")
+        self.assertEqual(self.exporter.chat_id, "dummy_token")
         mock_open.assert_called_once_with(".settings")
