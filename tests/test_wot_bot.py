55fd8e435d60ccd61aa175392c8a400e7ebecada
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-04 19:15:43 +0300

    Add some tests for WotBot class

--- tests/test_wot_bot.py
+++ tests/test_wot_bot.py
@@ -2,6 +2,7 @@ import os
 
 from unittest import mock, TestCase
 
+from config import Config
 from logger import CustomLogger
 from wot_bot import WotBot
 
@@ -37,3 +38,45 @@ class TestWotBot(TestCase):
         self.bot.check_gift_status()
         mock_check_logs.assert_called_once()
         mock_exit.assert_called_once()
+
+
+    @mock.patch.object(Config, "prepare_data")
+    @mock.patch.object(CustomLogger, "add_telegram_handler")
+    def test_config_props_with_th(self, mock_add_telegram_handler, mock_prepare_data):
+        mock_prepare_data.return_value = {
+            "driver": "Chrome",
+            "url": "dummy_url",
+            "username": "dummy_username",
+            "password": "dummy_password",
+            "token": "dummy_token"
+        }
+
+        self.bot.config_props()
+
+        self.assertEqual(self.bot.url, "dummy_url")
+        self.assertEqual(self.bot.username, "dummy_username")
+        self.assertEqual(self.bot.password, "dummy_password")
+        self.assertEqual(self.bot.token, "dummy_token")
+        self.assertEqual(self.bot.driver, "Chrome")
+        mock_prepare_data.assert_called_once()
+        mock_add_telegram_handler.assert_called_once_with("dummy_token")
+
+
+    @mock.patch.object(Config, "prepare_data")
+    def test_config_props_without_th(self, mock_prepare_data):
+        mock_prepare_data.return_value = {
+            "driver": "Chrome",
+            "url": "dummy_url",
+            "username": "dummy_username",
+            "password": "dummy_password",
+            "token": ""
+        }
+
+        self.bot.config_props()
+
+        self.assertEqual(self.bot.url, "dummy_url")
+        self.assertEqual(self.bot.username, "dummy_username")
+        self.assertEqual(self.bot.password, "dummy_password")
+        self.assertEqual(self.bot.token, "")
+        self.assertEqual(self.bot.driver, "Chrome")
+        mock_prepare_data.assert_called_once()
