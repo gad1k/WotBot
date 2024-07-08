dceb4d7dd3d98be298a5f499a40a31978acaadd3
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-09 00:21:49 +0300

    Add sepatate classes for browser and cookie

---
+++ browser.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeService, ChromeOptions, EdgeService, EdgeOptions, FirefoxService, FirefoxOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+class Browser:
+    def __init__(self, driver, headless=True):
+        self.engine = self.init_engine(driver, headless)
+        self.cookie = None
+
+
+    def init_engine(self, driver, headless):
+        if driver == "Chrome":
+            service = ChromeService(ChromeDriverManager().install())
+            options = self.set_options(headless, ChromeOptions())
+
+            engine = webdriver.Chrome(service=service, options=options)
+        elif driver == "Edge":
+            service = EdgeService(EdgeChromiumDriverManager().install())
+            options = self.set_options(headless, EdgeOptions())
+
+            engine = webdriver.Edge(service=service, options=options)
+        else:
+            service = FirefoxService(GeckoDriverManager().install())
+            options = self.set_options(headless, FirefoxOptions())
+
+            engine = webdriver.Firefox(service=service, options=options)
+
+        return engine
+
+
+    def set_options(self, headless, options):
+        if headless:
+            options.add_argument("--headless")
+
+        options.add_argument("--window-size=1280,900")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+
+        return options
+
+
+    def stop_browser(self):
+        if self.engine is not None:
+            self.engine.quit()
