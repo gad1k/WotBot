ddbecaab720f6560f61aecf99b7102dac2fd486a
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-24 23:53:23 +0300

    Change the log location

--- modules/main.py
+++ modules/main.py
@@ -1,7 +1,7 @@
 from wot_bot import WotBot
 
 
-bot = WotBot("../settings/config.json", "wot_bot.log")
+bot = WotBot("../settings/config.json", "../settings/wot_bot.log")
 bot.check_gift_status()
 bot.config_props()
 bot.get_gift()
