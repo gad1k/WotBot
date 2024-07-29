bf4f7802bccd68ba72a54ef4633b084c0a6dc519
Andrei Hryshechkin <andrei.hryshechkin@soname.de>
2024-07-29 20:05:35 +0300

    Simplify the testing of Browser class by introducing setUp and tearDown methods

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -11,25 +11,29 @@ from modules.cookie import Cookie
 
 class TestBrowser(TestCase):
     @mock.patch.object(Browser, "init_engine")
+    def setUp(self, mock_init_engine):
+        self.browser = Browser("Chrome", True)
+
+
+    def tearDown(self):
+        if self.browser.engine is not None:
+            self.browser.engine.quit()
+
+
     @mock.patch("selenium.webdriver.Chrome")
-    def test_check_engine_initialized(self, mock_web_driver, mock_init_engine):
-        browser = Browser("Chrome", True)
-        browser.engine = mock_web_driver
+    def test_check_engine_initialized(self, mock_web_driver):
+        self.browser.engine = mock_web_driver
 
-        self.assertTrue(browser.headless)
-        self.assertTrue(browser.check_engine_initialized())
-        mock_init_engine.assert_called_once_with("Chrome")
+        self.assertTrue(self.browser.headless)
+        self.assertTrue(self.browser.check_engine_initialized())
 
 
-    @mock.patch.object(Browser, "init_engine")
     @mock.patch("selenium.webdriver.Chrome")
-    def test_get_engine(self, mock_web_driver, mock_init_engine):
-        browser = Browser("Chrome", True)
-        browser.engine = mock_web_driver
+    def test_get_engine(self, mock_web_driver):
+        self.browser.engine = mock_web_driver
 
-        self.assertTrue(browser.headless)
-        self.assertEqual(browser.get_engine(), mock_web_driver)
-        mock_init_engine.assert_called_once_with("Chrome")
+        self.assertTrue(self.browser.headless)
+        self.assertEqual(self.browser.get_engine(), mock_web_driver)
 
 
     def test_init_engine_chrome(self):
@@ -38,12 +42,18 @@ class TestBrowser(TestCase):
         self.assertTrue(browser.headless)
         self.assertIsInstance(browser.engine, WebDriverChrome)
 
+        if browser.engine is not None:
+            browser.engine.quit()
+
 
     def test_init_engine_edge(self):
         browser = Browser("Edge", True)
 
         self.assertTrue(browser.headless)
         self.assertIsInstance(browser.engine, WebDriverEdge)
+
+        if browser.engine is not None:
+            browser.engine.quit()
 
 
     def test_init_engine_firefox(self):
@@ -52,15 +62,15 @@ class TestBrowser(TestCase):
         self.assertTrue(browser.headless)
         self.assertIsInstance(browser.engine, WebDriverFirefox)
 
+        if browser.engine is not None:
+            browser.engine.quit()
 
-    @mock.patch.object(Browser, "init_engine")
+
     @mock.patch.object(Cookie, "save")
-    def test_save_cookies(self, mock_save, mock_init_engine):
-        browser = Browser("Chrome", True)
-        browser.save_cookies()
+    def test_save_cookies(self, mock_save):
+        self.browser.save_cookies()
 
-        self.assertTrue(browser.headless)
-        mock_init_engine.assert_called_once_with("Chrome")
+        self.assertTrue(self.browser.headless)
         mock_save.assert_called_once()
 
 
@@ -76,47 +86,36 @@ class TestBrowser(TestCase):
         self.assertEqual(options.arguments[3], "--ignore-certificate-errors")
         mock_init_engine.assert_called_once_with("Chrome")
 
+        if browser.engine is not None:
+            browser.engine.quit()
 
-    @mock.patch.object(Browser, "init_engine")
-    def test_set_options_headless_true(self, mock_init_engine):
-        browser = Browser("Chrome", True)
-        options = browser.set_options(ChromeOptions(), browser.headless)
 
-        self.assertTrue(browser.headless)
+    def test_set_options_headless_true(self):
+        options = self.browser.set_options(ChromeOptions(), self.browser.headless)
+
+        self.assertTrue(self.browser.headless)
         self.assertEqual(options.arguments[0], "--headless")
         self.assertEqual(options.arguments[1], "--window-size=1280,900")
         self.assertEqual(options.arguments[2], "--disable-gpu")
         self.assertEqual(options.arguments[3], "--log-level=3")
         self.assertEqual(options.arguments[4], "--ignore-certificate-errors")
-        mock_init_engine.assert_called_once_with("Chrome")
 
 
-    @mock.patch.object(Browser, "init_engine")
-    def test_stop_engine(self, mock_init_engine):
-        browser = Browser("Chrome", True)
-        browser.stop_engine()
+    def test_stop_engine(self):
+        self.browser.stop_engine()
 
-        self.assertTrue(browser.headless)
-        mock_init_engine.assert_called_once_with("Chrome")
+        self.assertTrue(self.browser.headless)
 
 
-    @mock.patch.object(Browser, "init_engine")
     @mock.patch.object(Cookie, "use")
-    def test_use_cookies(self, mock_use, mock_init_engine):
-        browser = Browser("Chrome", True)
-
-        self.assertTrue(browser.headless)
-        self.assertTrue(browser.use_cookies())
-        mock_init_engine.assert_called_once_with("Chrome")
+    def test_use_cookies(self, mock_use):
+        self.assertTrue(self.browser.headless)
+        self.assertTrue(self.browser.use_cookies())
         mock_use.assert_called_once()
 
 
-    @mock.patch.object(Browser, "init_engine")
     @mock.patch.object(Cookie, "use", side_effect=FileNotFoundError)
-    def test_use_cookies_catch_file_not_found_error(self, mock_use, mock_init_engine):
-        browser = Browser("Chrome", True)
-
-        self.assertTrue(browser.headless)
-        self.assertFalse(browser.use_cookies())
-        mock_init_engine.assert_called_once_with("Chrome")
+    def test_use_cookies_catch_file_not_found_error(self, mock_use):
+        self.assertTrue(self.browser.headless)
+        self.assertFalse(self.browser.use_cookies())
         mock_use.assert_called_once()
